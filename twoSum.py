266057d11de7b07fbd6d5b39c59dade66106a1de
ouwais1922 <ouwaisszlaigi2@gmail.com>
2022-12-24 23:28:09 +0100

    two sum2

--- twoSum.py
+++ twoSum.py
@@ -4,7 +4,7 @@
     ##range(i+1,len(nums)) ===> j=1,j=2,j=3 (inner loop)
 #range(): generates a sequence of numbers starting from 0 (by default), 
 # and increments by 1 (also by default), until it reaches a specified upper bound (stop).
-
+#we check the first element with the remaining elements ...
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:

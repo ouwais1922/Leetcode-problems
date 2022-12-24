d47c55f7dfafcc65f6542928fcf82911377a1f6c
ouwais1922 <ouwaisszlaigi2@gmail.com>
2022-12-24 15:55:36 +0100

    roman to integer

--- romanToInteger.py
+++ romanToInteger.py
@@ -1,1 +1,15 @@
-print("hello world")
+def myConstant(r):
+    if(r == 'I'):
+        return 1
+    if(r == 'V'):
+        return 5
+    if(r == 'X'):
+        return 10
+    if(r == 'L'):
+        return 50
+    if(r == 'C'):
+        return 100
+    if(r == 'D'):
+        return 500
+    if(r == 'M'):
+        return 1000

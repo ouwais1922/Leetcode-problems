970fb9365a9e8736db1436a1d3574be92ec212f0
ouwais1922 <ouwaisszlaigi2@gmail.com>
2022-12-25 20:23:42 +0100

    without convert to string

--- palindrome.py
+++ palindrome.py
@@ -1,8 +1,29 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        if x < 0:
-            return False
+def isPalindrome( x: int) -> bool:
+     if x < 0:
+        return False
         toString = str(x)
         return toString == toString[::-1]
 
 # try without converted to a string
+
+print(isPalindrome(121))
+
+
+## without converting to string
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        reverseNumber = 0
+        firstValue = x
+        while x > 0:
+            lastDigit = x % 10
+            reverseNumber = reverseNumber * 10 + lastDigit
+            x = x // 10
+        return firstValue == reverseNumber
+
+# lets take : 121
+# lastDigit = 1
+# reverseNumber = 0 * 10 + 1 = 1
+# x = 121 // 10 = 12
